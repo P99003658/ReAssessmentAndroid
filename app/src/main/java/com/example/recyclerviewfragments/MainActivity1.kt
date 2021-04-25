e51773e7e7f3616e382e3e783940bb64bda4a313
P99003658 <78849248+P99003658@users.noreply.github.com>
2021-04-25 17:18:15 +0530

    Update MainActivity1.kt

--- app/src/main/java/com/example/recyclerviewfragments/MainActivity1.kt
+++ app/src/main/java/com/example/recyclerviewfragments/MainActivity1.kt
@@ -11,16 +11,15 @@ class MainActivity1(): AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main1)
-       // var adapter: RecyclerView.Adapter<Adapter.MyViewHolder>? = null
-        // var rview = findViewById<RecyclerView>(R.id.myrecyclerview)
-       //rview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        var rview = findViewById<RecyclerView>(R.id.myrecyclerview)
+       rview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         var data = ArrayList<UsersData>()
         data.add(UsersData("Name: Pankaj","Location: Bangalore","Track: SPS Java"))
         data.add(UsersData("Name: Bhanu","Location: Mysore","Track: SPS C#"))
         data.add(UsersData("Name: Ravi","Location: Mumbai","Track: Embedded"))
         data.add(UsersData("Name: Geethika","Location: Baroda","Track: Python"))
         data.add(UsersData("Name: Khanith","Location: Hyderabad","Track: SPS Java"))
-     //   adapter = Adapter()
+     adapter = Adapter()
         supportFragmentManager.beginTransaction().add(R.id.yellow_layout,MainFragment()).commit()
     }
 }
